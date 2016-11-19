99e8d215f5a4e6e68559a00d1c211aeba158796c
richardison <richard.ison@carleton.ca>
2016-11-19 01:06:57 -0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports =  {
-           apiKey: "AIzaSyD9CbjU8naQz_tsE6qrrlEU5e92laMPO1A",
-           authDomain: "fitbot-7777f.firebaseapp.com",
-           databaseURL: "https://fitbot-7777f.firebaseio.com",
-           storageBucket: "fitbot-7777f.appspot.com",
-           messagingSenderId: "290445597768"
-}
